57361d842eac107db447b0f2cc3f4fd666a4b0e7
Dominic Waithe <dominic_waithe@hotmail.com>
2019-11-08 15:28:37 +0000

    refining dataset generation homo atnd hetero

--- src/convert_voc_to_other.py
+++ src/convert_voc_to_other.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from numpy.random import RandomState
 import os
+from shutil import copyfile
 def create_retinaNet_single(datasets,datasets_size,path,path_on_server):
     
     
@@ -26,46 +27,42 @@ def create_retinaNet_single(datasets,datasets_size,path,path_on_server):
         #output as csv, in each directory.
         csvfileout = open(path_dir+'retina_train_dn_n'+str(dataset_size[0])+'.csv',"w")
         
-        
-       
-    
-        
         def read_data(datain,csvfileout,fclasses):
-                    while 1:
-                        line = datain.readline()
-                        if not line:
-                            break
+            while 1:
+                line = datain.readline()
+                if not line:
+                    break
 
-                        file_nm = line[:-1]
-                        file_path = path+dataset+"Annotations/"+file_nm+'.xml'
+                file_nm = line[:-1]
+                file_path = path+dataset+"Annotations/"+file_nm+'.xml'
 
-                        data =  open(file_path,"r")
-                        out_str = ""
-                        while 1:
-                            line = data.readline()
-                            if not line:
-                                break
-                            if line.find("<name>") >-1:
-                                im_class = line.split("<name>")[1].split("</name>")[0]
-                            if line.find("<width>") >-1:
-                                im_width = float(line.split("<width>")[1].split("</width>")[0])
-                            if line.find("height") >-1:
-                                im_height = float(line.split("<height>")[1].split("</height>")[0])
-                            if line.find("<bndbox>") > -1:
-                                xmin_str = data.readline()
-                                xmin = int(xmin_str.split("<xmin>")[1].split("</xmin>")[0])
-                                ymin_str = data.readline()
-                                ymin = int(ymin_str.split("<ymin>")[1].split("</ymin>")[0])
-                                xmax_str = data.readline()
-                                xmax = int(xmax_str.split("<xmax>")[1].split("</xmax>")[0])
-                                ymax_str = data.readline()
-                                ymax = int(ymax_str.split("<ymax>")[1].split("</ymax>")[0])
+                data =  open(file_path,"r")
+                out_str = ""
+                while 1:
+                    line = data.readline()
+                    if not line:
+                        break
+                    if line.find("<name>") >-1:
+                        im_class = line.split("<name>")[1].split("</name>")[0]
+                    if line.find("<width>") >-1:
+                        im_width = float(line.split("<width>")[1].split("</width>")[0])
+                    if line.find("height") >-1:
+                        im_height = float(line.split("<height>")[1].split("</height>")[0])
+                    if line.find("<bndbox>") > -1:
+                        xmin_str = data.readline()
+                        xmin = int(xmin_str.split("<xmin>")[1].split("</xmin>")[0])
+                        ymin_str = data.readline()
+                        ymin = int(ymin_str.split("<ymin>")[1].split("</ymin>")[0])
+                        xmax_str = data.readline()
+                        xmax = int(xmax_str.split("<xmax>")[1].split("</xmax>")[0])
+                        ymax_str = data.readline()
+                        ymax = int(ymax_str.split("<ymax>")[1].split("</ymax>")[0])
 
-                                out_str += path_on_server+dataset+"JPEGImages/"+file_nm+'.jpg,'
-                                out_str += str(xmin-1)+","+str(ymin-1)+","+str(xmax-1)+","+str(ymax-1)+','+im_class+"\n"
+                        out_str += path_on_server+dataset+"JPEGImages/"+file_nm+'.jpg,'
+                        out_str += str(xmin-1)+","+str(ymin-1)+","+str(xmax-1)+","+str(ymax-1)+','+im_class+"\n"
 
-                        csvfileout.write(out_str)
-                    fclasses.write(im_class+",0"+'\n');
+                csvfileout.write(out_str)
+            fclasses.write(im_class+",0"+'\n');
         read_data(datain,csvfileout,fclasses)
         datain.close()
         fclasses.close()
@@ -98,9 +95,11 @@ def create_retinaNet_global(params,datasets,datasets_size,path,path_on_server):
         #output as csv, in each directory.
         csvfileout_train = open(path+param['dataset_path']+param['rn_training_set_name']+'.csv',"w")
         csvfileout_test = open(path+param['dataset_path']+param['rn_testing_set_name']+'.csv',"w")
+        type_of_mixed = param['type']
         c=0
         d=0
 
+        
 
         for dataset,dataset_size in zip(datasets,datasets_size):
 
@@ -116,11 +115,19 @@ def create_retinaNet_global(params,datasets,datasets_size,path,path_on_server):
                     os.makedirs(directory)
 
 
+                datain_train =  open(path_in+'train_n'+str(dataset_size[0])+'.txt',"r")
+                out_str1 = ""
 
+                #read_data(datain_train,csvfileout_train,fclasses,d)
+                datain_test =  open(path_in+'test_n'+str(dataset_size[1])+'.txt',"r")
+                out_str2 = ""
+                #read_data(datain_test,csvfileout_test,None,d)
+                e = 0
+                for out_str, input_file, csv_out in zip([out_str1,out_str2],[datain_train,datain_test],[csvfileout_train,csvfileout_test]):
 
-                def read_data(datain,csvfileout,fclasses,d):
+                        #def read_data(datain,csvfileout,fclasses,d):
                         while 1:
-                            line = datain.readline()
+                            line = input_file.readline()
                             if not line:
                                 break
 
@@ -150,23 +157,23 @@ def create_retinaNet_global(params,datasets,datasets_size,path,path_on_server):
                                     ymax = int(ymax_str.split("<ymax>")[1].split("</ymax>")[0])
 
                                     out_str += path_on_server+dataset+"JPEGImages/"+file_nm+'.jpg,'
-                                    out_str += str(xmin-1)+","+str(ymin-1)+","+str(xmax-1)+","+str(ymax-1)+','+im_class+"\n"
+                                    if type_of_mixed == "hetero":
+                                        out_str += str(xmin-1)+","+str(ymin-1)+","+str(xmax-1)+","+str(ymax-1)+','+im_class+"\n"
+                                    else:
+                                        out_str += str(xmin-1)+","+str(ymin-1)+","+str(xmax-1)+","+str(ymax-1)+",cell - multiple types\n"
 
-                            csvfileout.write(out_str)
+                            csv_out.write(out_str)
                         if fclasses !=None:
-                            fclasses.write(im_class+","+str(d)+'\n');
-
-
-                datain_train =  open(path_in+'train_n'+str(dataset_size[0])+'.txt',"r")
-                out_str = ""
-                read_data(datain_train,csvfileout_train,fclasses,d)
-                datain_test =  open(path_in+'test_n'+str(dataset_size[1])+'.txt',"r")
-                out_str = ""
-                read_data(datain_test,csvfileout_test,None,d)
+                            if type_of_mixed == "hetero" and e == 0:
+                                fclasses.write(im_class+","+str(d)+'\n');
+                            e+=1
                 d+=1
             c+=1
+        if type_of_mixed == "homo":
+            fclasses.write("cell - multiple types,0\n");
         csvfileout_train.close()
         csvfileout_test.close()
+        fclasses.close()
         datain_test.close()
         datain_train.close()
 def convert_to_yolo_format(xmin,xmax,ymin,ymax,im_height,im_width):
@@ -326,11 +333,9 @@ def create_YOLO_single(datasets,datasets_size,datasets_class,path,path_on_server
         f = open(path_dir+'obj_'+str(dataset.split("/")[0])+'.names',"w")
         f.write(dataset_class+"\n")
         f.close()
-def create_YOLO_global(params, datasets,datasets_size,datasets_class,path,path_on_server,backup_path):
-    
-    """ Generate global list
-        This is where we generate the ImageSets for the YOLO algorithm, from those defined for the Faster-RCNN.
-        A global list is a list of images which encompasses multiple different classes."""
+def create_FasterRCNN_mixed_dataset(params, datasets,datasets_size,datasets_class,path,path_on_server,backup_path):
+
+
     for ind in params:
         param = params[ind]
         global_path = param['dataset_path']
@@ -338,6 +343,9 @@ def create_YOLO_global(params, datasets,datasets_size,datasets_class,path,path_o
         global_test_name = param['dn_mixed_class_name']
         training_set_name = param['dn_training_set_name']
         testing_set_name = param['dn_testing_set_name']
+        type_of_mixed = param['type']
+        
+    
 
 
 
@@ -349,8 +357,20 @@ def create_YOLO_global(params, datasets,datasets_size,datasets_class,path,path_o
             glb_datasets_size.append(datasets_size[index_to_use])
             glb_datasets_class.append(datasets_class[index_to_use])
         directory = (path+global_path+"ImageSets/Main/")
+
         if not os.path.exists(directory):
             os.makedirs(directory)
+
+        directory = (path+global_path+"JPEGImages/")
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        directory = (path+global_path+"Annotations/")
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        
+
         outF1 = open(path+global_path+"ImageSets/Main/"+"train_n.txt", "w")
         outF2 = open(path+global_path+"ImageSets/Main/"+"test_n.txt", "w")
         for dataset,dataset_size in zip(glb_datasets,glb_datasets_size):
@@ -366,31 +386,116 @@ def create_YOLO_global(params, datasets,datasets_size,datasets_class,path,path_o
         outF2.close()
 
 
+        
+        for dataset,dataset_size in zip(glb_datasets,glb_datasets_size):
+            path_dir = path+dataset
+            path_in = path_dir+"ImageSets/Main/"    
+            data1 = open(path_in+"train_n"+str(int(dataset_size[0]))+".txt","r")
+            data2 = open(path_in+"test_n"+str(int(dataset_size[1]))+".txt","r")
+
+            for data in [data1,data2]:
+                while 1:
+                    line = data.readline()
+                    if not line:
+                        break
+                
+                    src = path_dir+ "JPEGImages/"+line[:-1]+'.jpg'
+                    dst = path+global_path+"JPEGImages/"+line[:-1]+'.jpg'
+                    copyfile(src, dst)
+                    src = path_dir+ "Annotations/"+line[:-1]+'.xml'
+                    dst = path+global_path+"Annotations/"+line[:-1]+'.xml'
+                    copyfile(src, dst)
+                    if type_of_mixed == "homo":
+                        xml_data =  open(dst,"r")
+                        out_str = ""
+                        while 1:
+                            line_xml = xml_data.readline()
+                            if not line_xml:
+                                break
+                            if line_xml.find("<name>") >-1:
+                                preline = line_xml.split("<name>")[0]
+                                postline = line_xml.split("</name>")[1]
+                                line_xml = preline+"<name>cell - multiple types</name>"+postline
+                            out_str += line_xml
+                        xml_data.close()
+                        xml_data = open(dst,"w")
+                        xml_data.writelines(out_str)  
+                        xml_data.close()                  
+
+     
+
+def create_YOLO_mixed_hetero_class(params, datasets,datasets_size,datasets_class,path,path_on_server,backup_path):
+    """ Generate global list
+        This is where we generate the ImageSets for the YOLO algorithm, from those defined for the Faster-RCNN.
+        A global list is a list of images which encompasses multiple different classes."""
+    for ind in params:
+        param = params[ind]
+        global_path = param['dataset_path']
+        indices_to_use = param['indices_to_use']
+        global_test_name = param['dn_mixed_class_name']
+        training_set_name = param['dn_training_set_name']
+        testing_set_name = param['dn_testing_set_name']
+        type_of_mixed = param['type']
+        
+    
+
+
+
+        glb_datasets = []
+        glb_datasets_size = []
+        glb_datasets_class = []
+        for index_to_use in indices_to_use:
+            glb_datasets.append(datasets[index_to_use]) 
+            glb_datasets_size.append(datasets_size[index_to_use])
+            glb_datasets_class.append(datasets_class[index_to_use])
+        
+
+        directory = (path+global_path+"labels")
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        
+
+
         f1 =  open(path+global_path+training_set_name+'.txt',"w")
         f2 =  open(path+global_path+testing_set_name+'.txt',"w")
 
+        label_count = 0
         for dataset,dataset_size in zip(glb_datasets,glb_datasets_size):
-                path_dir = path+dataset
-                path_in = path_dir+"ImageSets/Main/"    
-                data1 = open(path_in+"train_n"+str(int(dataset_size[0]))+".txt","r")
+            path_dir = path+dataset
+            path_in = path_dir+"ImageSets/Main/"    
+            data1 = open(path_in+"train_n"+str(int(dataset_size[0]))+".txt","r")
+            data2 = open(path_in+"test_n"+str(int(dataset_size[1]))+".txt","r")
 
-
+            for data in [data1,data2]:
                 while 1:
-                    line = data1.readline()
+                    line = data.readline()
                     if not line:
                         break
-                    f1.write(path_on_server+dataset+"JPEGImages/"+line[:-1]+'.jpg\n')
+                    f1.write(path_on_server+global_path+"JPEGImages/"+line[:-1]+'.jpg\n')
+                                        
+                    src = path_dir+ "labels/"+line[:-1]+'.txt'
+                    dst = path+global_path+"labels/"+line[:-1]+'.txt'
+                   
                     
+                    label1 =  open(src,"r")
+                    label2 =  open(dst,"w")
+                    while 1:
+                        label_line = label1.readline()
+                        if not label_line:
+                            break
+                        labels_ = label_line.split(" ")
+                        labels_[0] = str(label_count)
+                        out_label = " ".join(labels_)
+                        label2.write(out_label)
+                    label1.close()
+                    label2.close()
 
-                path_dir = path+dataset  
-                path_in = path_dir+"ImageSets/Main/" 
-                data2 = open(path_in+"test_n"+str(int(dataset_size[1]))+".txt","r")
+            if type_of_mixed == "hetero":
+                label_count +=1                        
 
-                while 1:
-                    line = data2.readline()
-                    if not line:
-                        break
-                    f2.write(path_on_server+dataset+"JPEGImages/"+line[:-1]+'.jpg\n')
+
+
 
         f1.close()
         data1.close()
@@ -400,19 +505,24 @@ def create_YOLO_global(params, datasets,datasets_size,datasets_class,path,path_o
         f = open(path+global_path+global_test_name+'.data',"w")
         f1 = open(path+global_path+global_test_name+'.names',"w")
         c = 0
-        for dataset,dataset_size,dataset_class in zip(glb_datasets,glb_datasets_size,glb_datasets_class):
-            path_dir = path+dataset
-            c+=1
 
-
-            f1.write(dataset_class+"\n")
-        f.write("classes = "+str(c)+"\n")
+        
+        if type_of_mixed == "hetero":
+            for dataset,dataset_size,dataset_class in zip(glb_datasets,glb_datasets_size,glb_datasets_class):
+                path_dir = path+dataset
+                c+=1
+                f1.write(dataset_class+"\n")
+            f.write("classes = "+str(c)+"\n")
+        else:
+            f1.write("cell - multiple types"+"\n")
+            f.write("classes = "+str(1)+"\n")
         f.write("train = "+path_on_server+global_path+training_set_name+'.txt'+"\n")
         f.write("valid = "+path_on_server+global_path+testing_set_name+'.txt'+"\n")
         f.write("names = "+path_on_server+global_path+global_test_name+'.names'+"\n")
-        f.write("backup = "+backup_path+'darknet/'+global_path)
+        f.write("backup = "+backup_path+'darknet/'+global_path.split("/")[0]+'/')
 
         f.close()
+
 
 def return_dataset_spec(path_to_spec):
     """opens spec file and returns contents as arrays"""
@@ -513,12 +623,17 @@ def return_mixed_dataset_spec(path_to_spec):
            
             if line[0:4]=="dir=":
                 param['dataset_path'] = str(line[4:].strip("\""))
-
             line = next_line()
+            
+            val = "type="
+            if line[0:val.__len__()]==val:
+                param[val.strip("=")] = line[val.__len__():].strip('\"').strip('\'')
+            line = next_line()
+
             val = "indices_to_use="
             if line[0:val.__len__()]==val:
                 param['indices_to_use'] = np.array(line[val.__len__():].strip('\"').split(',')).astype(np.int)
-
+            
             line = next_line()
             val = "dn_mixed_class_name="
             if line[0:val.__len__()]==val:
@@ -545,7 +660,8 @@ def return_mixed_dataset_spec(path_to_spec):
                 param[val.strip("=")] = line[val.__len__():].strip('\"')
             
             params[indices[-1]] = param
-            if param.__len__() != 7:
+            if param.__len__() != 8:
+                
                 print('fail. One of your param entries is not complete: mixed dataset',indices[-1])
                 return False,False;
             
